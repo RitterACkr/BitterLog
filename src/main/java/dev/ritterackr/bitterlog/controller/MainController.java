926dd867553c063964dd5950d8e6b1047de4d604
RitterACkr <ritter.ackr@gmail.com>
2026-05-18 16:27:05 +0900

    fix: エディタのTABスペースを半角スペース4マスに変更

--- src/main/java/dev/ritterackr/bitterlog/controller/MainController.java
+++ src/main/java/dev/ritterackr/bitterlog/controller/MainController.java
@@ -240,6 +240,17 @@ public class MainController implements Initializable {
                             "window.scrollTo(0, scrollableHeight * " + scrollRatio + ");"
             );
         });
+        editorArea.addEventFilter(javafx.scene.input.KeyEvent.KEY_PRESSED, event -> {
+            if (event.getCode() == javafx.scene.input.KeyCode.TAB) {
+                event.consume();
+                editorArea.insertText(editorArea.getCaretPosition(), "    ");
+            }
+        });
+        editorArea.setOnKeyPressed(event -> {
+            if (event.isControlDown() && event.getCode() == javafx.scene.input.KeyCode.V) {
+                pasteImageFromClipboard();
+            }
+        });
 
         // メモカテゴリComboBoxの設定
         memoCategoryComboBox.setCellFactory(list -> new ListCell<>() {
